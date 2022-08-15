fb4ad406334bed3c0f7ad26030a368742104dd16
Maykon Freitas <maykon17programmer@gmail.com>
2022-08-15 15:07:34 -0300

    feat: criado a função update do ProductsServices

--- services/ProductsService.js
+++ services/ProductsService.js
@@ -23,8 +23,23 @@ const create = async (name) => {
   return { id: insertId, name };
 };
 
+const update = async (id, name) => {
+  // Validadando o ID
+  const productByID = await ProductsModel.getById(id);
+  if (productByID.length === 0) return { code: 404, message: 'Product not found' };
+  // Validando o Nome
+  const validate = ProductValidate.createAndUpdate(name);
+  const { code, message } = validate;
+  if (code && message) {
+    return { code, message };
+  }
+  const product = await ProductsModel.update(name);
+  return product;
+};
+
 module.exports = {
   getAll,
   getById,
   create,
+  update,
 };
