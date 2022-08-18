35a7fd449dea6991e4b0299a7a149ca46a716d33
Maykon Freitas <maykon17programmer@gmail.com>
2022-08-18 17:59:39 -0300

    feat: criado a rota /sales/:id com o method delete para deletar vendas

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -8,5 +8,6 @@ const SalesProductsController = require('../controllers/SalesProductsController'
 routes.get('/', SalesController.getAll);
 routes.get('/:id', SalesController.getById);
 routes.post('/', SalesProductsController.create);
+routes.delete('/:id', SalesController.exclude);
 
 module.exports = routes;
