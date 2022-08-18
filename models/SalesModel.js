c92ea5ec56758e0f494ed2208c5b92dcd1a49c39
Maykon Freitas <maykon17programmer@gmail.com>
2022-08-18 14:12:55 -0300

    test/feat: criado testes para a função getAll do  SalesModel | criado função getll do arquivo SalesModel

--- models/SalesModel.js
+++ models/SalesModel.js
@@ -7,6 +7,20 @@ const create = async () => {
   return response;
 };
 
+const getAll = async () => {
+  const [response] = await connection.execute(
+  `
+    SELECT sale.id AS "saleId", sale.date, sale_product.product_id, sale_product.quantity
+    FROM StoreManager.sales AS sale
+    INNER JOIN StoreManager.sales_products AS sale_product
+    ON sale.id = sale_product.sale_id
+    ORDER BY sale.id ASC, sale_product.product_id
+  `,
+  );
+  return response;
+};
+
 module.exports = {
   create,
+  getAll,
 };
--- tests/unit/models/SalesModel.test.js
+++ tests/unit/models/SalesModel.test.js
@@ -23,3 +23,55 @@ describe('Ao Cadastrar uma nova venda(Sale) - SalesModel', () => {
     expect(response).to.be.a('object');
   });
 });
+
+describe('Ao Listar todas as vendas - SalesModel', () => {
+  const sales = [
+    [
+      {
+        saleId: 1,
+        date: "2021-09-09T04:54:29.000Z",
+        productId: 1,
+        quantity: 2
+      },
+      {
+        saleId: 1,
+        date: "2021-09-09T04:54:54.000Z",
+        productId: 2,
+        quantity: 2
+      }
+    ]
+  ];
+
+  before(async () => {
+    sinon.stub(connection, 'execute').resolves(sales);
+  });
+
+  after(async () => { 
+    connection.execute.restore();
+  });
+
+  it('Retorna uma lista com as vendas', async () => {
+    const response = await SalesModel.getAll();
+    expect(response).to.be.a('array');
+  });
+
+  it('Retorna um lista com vendas com a propriedade "saleId"', async () => {
+    const response = await SalesModel.getAll();
+    response.forEach((sale) => expect(sale).to.have.property('saleId'));
+  });
+
+  it('Retorna um lista com vendas com a propriedade "date"', async () => {
+    const response = await SalesModel.getAll();
+    response.forEach((sale) => expect(sale).to.have.property('date'));
+  });
+
+  it('Retorna um lista com vendas com a propriedade "productId"', async () => {
+    const response = await SalesModel.getAll();
+    response.forEach((sale) => expect(sale).to.have.property('productId'));
+  });
+
+  it('Retorna um lista com vendas com a propriedade "quantity"', async () => {
+    const response = await SalesModel.getAll();
+    response.forEach((sale) => expect(sale).to.have.property('quantity'));
+  });
+});
